c0f8fc743869a560a181038781fd3a34a30dd3f7
janderkkotlarski <j.d.kotlarski@gmx.com>
2016-04-13 01:09:49 +0200

    new

--- Dungeon_Texter_V0-3.cpp
+++ Dungeon_Texter_V0-3.cpp
@@ -60,6 +60,8 @@ void check_and_move(std::vector <std::vector<char>>& dungeon, std::vector <int>&
 	
 	const std::vector <int> vect{dir_to_vect()};
 	
+	std::cout << vect[0] << ':' << vect[1];
+	
 	const std::vector <int> next{(player[0] + vect[0] + side_length_1) % side_length_1,
 								 (player[1] + vect[1] + side_length_2) % side_length_2};
 	
